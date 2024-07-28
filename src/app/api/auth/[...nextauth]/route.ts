b9fd7b7c3b9b32b2af749dc33fba668c9a4ec625
fightlazy <vvibulucknut17@gmail.comgit config --global user.name fightlazygit config --global user.email vvibulucknut17@gmail.com>
2024-07-29 00:53:24 +0700

    fix: route & auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,68 +1,7 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
+// src/app/api/auth/[...nextauth]/route.ts
+import NextAuth from "next-auth/next";
+import { authOptions } from "@/lib/authOptions";
 
-import CredentialsProvider from "next-auth/providers/credentials";
-import prisma from "../../../../../lib/prisma";
-import { comparePassword } from "@/lib/utils";
+const handler = NextAuth(authOptions);
 
-export const authOptions: NextAuthOptions = {
-  secret: process.env.NEXTAUTH_SECRET,
-  providers: [
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        email: {
-          label: "email",
-          type: "email",
-        },
-        password: {
-          label: "password",
-          type: "password",
-        },
-      },
-      async authorize(credentials, req) {
-        const user = await prisma.company.findFirst({
-          where: {
-            email: credentials?.email,
-          },
-        });
-
-        if (!user) {
-          return null;
-        }
-
-        const isMatch = await comparePassword(
-          credentials?.password!!,
-          user.password
-        );
-
-        if (isMatch) {
-          return user;
-        }
-
-        return null;
-      },
-    }),
-  ],
-  pages: {
-    signIn: "/auth/signin",
-    newUser: "/auth/signup",
-  },
-  callbacks: {
-    jwt({ token, account, user }) {
-      if (account) {
-        token.id = user.id;
-      }
-
-      return token;
-    },
-    async session({ session, token, user }) {
-      session.user.id = token.id as string;
-
-      return session;
-    },
-  },
-};
-
-const handler = NextAuth(authOptions)
-
-export {handler as GET, handler as POST}
+export { handler as GET, handler as POST };
